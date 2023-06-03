4a63399246e44eebbb1453aad288126eedf60b94
Gaurav Patidar <patidargaurav987>
2023-06-03 21:22:20 +0530

    phone auth alert dialog made

--- app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.mycompany.myapp;
 
-import android.app.*;
 import android.os.*;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
 import android.graphics.*;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.google.android.material.snackbar.Snackbar;
 import io.reactivex.Completable;
 import java.util.concurrent.*;
 import io.reactivex.android.schedulers.*;
@@ -14,12 +19,12 @@ import android.content.*;
 import androidx.appcompat.app.*;
 import androidx.appcompat.widget.*;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 	Toolbar ttb;
+	private String phone;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.main);
@@ -28,10 +33,43 @@ public class MainActivity extends AppCompatActivity
 	}
 
 	@Override
-	protected void attachBaseContext(Context newBase)
-	{
+	protected void attachBaseContext(Context newBase) {
 		// TODO: Implement this method
 		super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
 	}
 
+	public void loginWithPhone(View v) {
+		AlertDialog.Builder ab = new AlertDialog.Builder(this);
+		ab.setTitle("Login");
+		ab.setMessage("Enter your phone number");
+		View vv = LayoutInflater.from(getBaseContext()).inflate(R.layout.layout_phone_edit, null);
+		final EditText edPhone = vv.findViewById(R.id.edt_phone);
+
+		ab.setView(vv);
+
+		ab.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int id) {
+				if (edPhone.getText().toString().length() == 10) {
+					phone = edPhone.getText().toString();
+				} else {
+					Snackbar s = Snackbar.make(findViewById(R.id.rel), "Invalid Phone Number", Snackbar.LENGTH_LONG);
+					View snackbarView = s.getView();
+					snackbarView.setPadding(0, 0, 0, 32);
+					s.setTextColor(Color.parseColor("#50ffffff"));
+					s.show();
+				}
+			}
+		});
+		ab.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int id) {
+			}
+		});
+		ab.show();
+	}
+
+	public void loginWithGoogle(View v) {
+
+	}
 }
